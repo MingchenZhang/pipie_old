01b1b7d36cd8afd28d6d72c4aa16c64eb049db22
Mingchen Zhang <zmc9499@gmail.com>
2017-05-23 21:58:13 -0400

    rebuild socket

--- demo/conn_and_receive.py
+++ demo/conn_and_receive.py
@@ -28,18 +28,18 @@ if int(peer_addr[3]) == 0:
     print('Instructed to directly connect. Connecting to: ')
     peerInfo = (peer_addr[0], int(peer_addr[1]))
     print(peerInfo)
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(("", localPort))
     retry_time = 1
     if int(peer_addr[4]) == 0:
         # local nat is a port preservation nat, we could try multiple times
-        retry_time = 100
+        retry_time = 1000
         # if local is not port preservation nat, only one try is needed (more connection would use other port)
         # and if remote computer reject the connection right away, there is no way to establish tcp connection.
     for i in range(0, retry_time):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             sock.connect(peerInfo)
+            sock.bind(("", localPort))
             print("connect")
             sock.send(str(random.randint(1000, 9999)).encode())
             print('received: ' + sock.recv(1024).decode())
